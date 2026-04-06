28163faee803f6b821b0dc8bf56e9a5e44cae00a
Zulva Husni Muntaha <156577936+Zhm46@users.noreply.github.com>
2026-04-07 00:16:17 +0700

    Update reject.php

--- source code/reject.php
+++ source code/reject.php
@@ -3,8 +3,6 @@ include 'koneksi.php';
 
 $id = $_GET['id'];
 
-// ubah status booking
 mysqli_query($koneksi, "UPDATE booking SET status='rejected' WHERE id='$id'");
 
-// redirect balik
 header("Location: booking_admin.php");
